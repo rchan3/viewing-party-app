66b8d59159dbb74e0f99f23acc6420725d0ce3f5
Rachel Chan <rachie.chan@gmail.com>
2019-10-11 17:10:02 -0400

    router get for passport authentication

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
-var passport = require
+var passport = require('passport');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/auth/google', passport.authenticate(
+  'google', {
+    successRedirect: '/',
+    failureRedirect: '/'
+  }));
 
 module.exports = router;

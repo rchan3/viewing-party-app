10873d762c9d1d0ef111dd19f0fc471b361008b9
Rachel Chan <rachie.chan@gmail.com>
2019-10-15 17:22:54 -0400

    fix index router

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {user:req.user, title: `Viewing Parties`});
 });
 
 router.get('/auth/google', passport.authenticate(
-  'google', {
-    successRedirect: '/',
-    failureRedirect: '/'
-  }));
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect : '/',
+    failureRedirect : '/'
+  }
+));
+
+router.get('/logout', function(req,res) {
+  req.logout();
+  res.redirect('/');
+})
 
 module.exports = router;

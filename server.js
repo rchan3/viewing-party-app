a6ee76cefd20aa402fdaf775dbf3a6d3f9cc6744
Rachel Chan <rachie.chan@gmail.com>
2019-10-16 16:02:48 -0400

    methodoverride

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var passport = require('passport');
 var logger = require('morgan');
+let methodOverride = require('method-override');
 require('dotenv').config();
 require('./config/database');
 require('./config/passport')
@@ -31,6 +32,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));  
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
